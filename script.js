fa8bf2aacc8ed6f4712dc6b9eee8ddfa7c1b5916
aman8387 <amansoni8387@gmail.com>
2024-10-01 13:57:02 +0530

    message

---
+++ script.js
@@ -0,0 +1,6 @@
+var hover = document.querySelector(".nav-singin");
+var nav = document.querySelector(".navbar .nav-hover");
+
+hover.addEventListener("mousemove", function() {
+    nav.style.opacity = 1;
+});
